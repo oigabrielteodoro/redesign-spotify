b6daf7e00da1eac8f700c0675af190e96f9e578c
oigabrielteodoro <oi@gabrielteodoro.com>
2021-04-03 20:00:46 -0300

    feat: added Email validation in schema to SignIn

--- src/pages/SignIn/schemas/getSignInSchema.ts
+++ src/pages/SignIn/schemas/getSignInSchema.ts
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 
 export default function getSignInSchema() {
   const schema = Yup.object({
-    email: Yup.string().required('Email is required'),
+    email: Yup.string().email('Invalid email').required('Email is required'),
     password: Yup.string().min(6, 'Password must be at least 6 digits'),
   });
 
